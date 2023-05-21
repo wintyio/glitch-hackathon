ba249c9de375334cc00c9ce21d2dbf8fb4bcdb74
yucheon6000 <yucheon6000@gmail.com>
2023-05-21 10:15:44 +0900

    배포

--- frontend/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { gameInfo } from '../slices/gameInfo';
-
-export default configureStore({
-    reducer: {
-        gameInfo: gameInfo.reducer
-    },
-});
